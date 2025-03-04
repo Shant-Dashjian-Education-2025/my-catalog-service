b9a9c059feb6c4005845c4825c05ab9d1935f50d
Shant Dashjian <shantgdashjian@gmail.com>
2025-03-04 22:17:22 +0400

    Init commit

---
+++ src/main/java/com/polarbookshop/mycatalogservice/MyCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.mycatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCatalogServiceApplication.class, args);
+	}
+
+}
